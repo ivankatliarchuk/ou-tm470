1f4c970573401bd9fdd6fdc419af09c85cd57f1c
Matthew Dutton <dutton.matt@gmail.com>
2018-08-03 12:50:19 +0100

    Auto Sync and Run

--- serviceChain/syncAndRun.sh
+++ serviceChain/syncAndRun.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-git add ../; git commit -m "Debugging persistence"; git push; go get github.com/mmd93ee/ou-tm470/dataPersist
+git add ../; git commit -m "Auto Sync and Run"; git push
+go get github.com/mmd93ee/ou-tm470/dataPersist
+go get github.com/mmd93ee/ou-tm470/webServer
 go build serviceChain.go; ./serviceChain
