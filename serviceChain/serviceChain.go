ff3cf444b9660a046e6a875d1c2593ff9c5b72a8
Matthew Dutton <dutton.matt@gmail.com>
2018-09-13 17:36:03 +0100

    Auto Sync and Run

--- serviceChain/serviceChain.go
+++ serviceChain/serviceChain.go
@@ -306,11 +306,11 @@ func writeServiceEventHandler(w http.ResponseWriter, r *http.Request) {
 	var newServiceEvent ServiceEvent
 
 	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(newServiceEvent); err != nil {
+	if err := decoder.Decode(&newServiceEvent); err != nil {
 		http.Error(w, "ERROR: Unable to decode data payload: "+err.Error(), 400)
 		return
 	}
-
+	log.Println(newServiceEvent.Identifier)
 	defer r.Body.Close()
 
 	// Generate block
