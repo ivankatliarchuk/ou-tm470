669db03c438728d34b27e8d9972b6802aa14b2b0
Matthew Dutton <dutton.matt@gmail.com>
2018-09-15 20:28:11 +0100

    Auto Sync and Run

--- serviceChain/loadEventData.sh
+++ serviceChain/loadEventData.sh
@@ -1,3 +1,7 @@
 
 
-curl -X POST --data @./md5589_service_test_data.json -H "Content-Type: application/json" http://localhost:8000/serviceevent/add/
+curl -X POST --data @./md5589_service_test_data_1.json -H "Content-Type: application/json" http://localhost:8000/serviceevent/add/
+curl -X POST --data @./md5589_service_test_data_2.json -H "Content-Type: application/json" http://localhost:8000/serviceevent/add/
+curl -X POST --data @./md5589_service_test_data_3.json -H "Content-Type: application/json" http://localhost:8000/serviceevent/add/
+curl -X POST --data @./md5589_service_test_data_4.json -H "Content-Type: application/json" http://localhost:8000/serviceevent/add/
+curl -X POST --data @./md5589_service_test_data_5.json -H "Content-Type: application/json" http://localhost:8000/serviceevent/add/
